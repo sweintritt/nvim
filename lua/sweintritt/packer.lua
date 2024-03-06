2a02ca10cdec63aa34eb392ae54be0a9f2782aba
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2024-03-06 05:43:09 +0100

    Added rose-pine theme

--- lua/sweintritt/packer.lua
+++ lua/sweintritt/packer.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+--- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -12,4 +12,12 @@ return require('packer').startup(function(use)
 	-- or                            , branch = '0.1.x',
 	requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use({
+	'rose-pine/neovim',
+	as = 'rose-pine',
+	config = function()
+		vim.cmd('colorscheme rose-pine')
+	end
+  })
 end)
