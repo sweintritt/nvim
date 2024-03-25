4b6f7b25c24e05267d915579ec4feabd13a64a73
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2024-03-25 05:50:10 +0100

    Installed lsp plugnis

--- lua/sweintritt/packer.lua
+++ lua/sweintritt/packer.lua
@@ -26,4 +26,28 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+ 
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-buffer'},
+    	          {'hrsh7th/cmp-path'},
+	          {'hrsh7th/cmp-nvim-lua'},
+	          {'saadparwaiz1/cmp_luasnip'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+	          {'rafamadriz/friendly-snippets'},
+	  }
+  }
+
 end)
