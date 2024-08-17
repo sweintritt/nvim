9bcf28282bca5ec7c434dcae4125a7376d77f555
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2024-08-17 06:22:22 +0200

    Removed some mappings already defined by neo-/lazyvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,14 +16,14 @@ vim.keymap.set("n", "<C-x>", vim.cmd.BufferLineCycleNext)
 vim.keymap.set("n", "<C-y>", vim.cmd.BufferLineCyclePrev)
 
 -- Easy window navigation
-vim.keymap.set("n", "<C-up>", "<C-w>k")
-vim.keymap.set("n", "<C-down>", "<C-w>j")
-vim.keymap.set("n", "<C-left>", "C-w>h")
-vim.keymap.set("n", "<C-right>", "C-w>l")
-vim.keymap.set("n", "<C-k>", "C-w>k")
-vim.keymap.set("n", "<C-j>", "C-w>j")
-vim.keymap.set("n", "<C-h>", "C-w>h")
-vim.keymap.set("n", "<C-l>", "C-w>l")
+-- vim.keymap.set("n", "<C-up>", "<C-w>k")
+-- vim.keymap.set("n", "<C-down>", "<C-w>j")
+-- vim.keymap.set("n", "<C-left>", "C-w>h")
+-- vim.keymap.set("n", "<C-right>", "C-w>l")
+-- vim.keymap.set("n", "<C-k>", "C-w>k")
+-- vim.keymap.set("n", "<C-j>", "C-w>j")
+-- vim.keymap.set("n", "<C-h>", "C-w>h")
+-- vim.keymap.set("n", "<C-l>", "C-w>l")
 
 -- Easy help navigation
 -- follow links
