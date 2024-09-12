5269274f93e449d092524a7b64f561ccf808274b
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2024-09-12 21:50:57 +0200

    Updated and disabled some plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,5 @@
 return {
   { "lukas-reineke/headlines.nvim", enabled = false },
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
