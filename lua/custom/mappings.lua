173bb95b00797b27a164f3c6bad0dbb7bc9fcdba
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2024-12-02 13:48:40 +0100

    Moved settings from init.lua to mappings.lua

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,5 +1,10 @@
 -- My custom NeoVim mappings additional to kickstart
 
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
 -- Disable arrow keys in normal mode
 vim.keymap.set("n", "<left>",  '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
