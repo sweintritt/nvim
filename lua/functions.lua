40ed5ddc04283ad10f0fdd0e02a652767ed81f26
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2025-02-14 13:21:33 +0100

    Small cleanup

--- lua/functions.lua
+++ lua/functions.lua
@@ -13,24 +13,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 
 --[[
-function! NextBuffer()
-    if !exists("b:NERDTree")
-        :bn
-    endif
-endfunction
-]]
---
-
---[[
-function! PreviousBuffer()
-    if !exists("b:NERDTree")
-        :bp
-    endif
-endfunction
-]]
---
-
---[[
 function! CleanUpTheFile()
     call feedkeys("gg=G")     " Fix indent
     call feedkeys("2\<C-o>")  " Jump back to steps in the jump list, which should bring you to where
@@ -55,28 +37,16 @@ endfunction
 
 function AddLogEntry()
 	-- Insert the formatted date
-	local date = os.date("# %A %d.%m.%Y\n\n\n")
-	--vim.api.nvim_put( os.date("# %A %d.%m.%Y\n\n\n"), "l", true, true)
+	vim.api.nvim_put({ os.date("# %A %d.%m.%Y") .. "\n\n\n" }, "l", true, true)
 	-- Save the current cursor position
-	--local position = vim.api.nvim_win_get_cursor(0)
+	local position = vim.api.nvim_win_get_cursor(0)
 	-- Insert an empty line after the date
-	-- vim.api.nvim_put({ "_" }, "l", true, true)
+	vim.api.nvim_put({ "_" }, "l", true, true)
 	-- Restore the cursor position
-	--vim.api.nvim_win_set_cursor(0, position)
+	vim.api.nvim_win_set_cursor(0, position)
 	-- Enter insert mode
-	--vim.cmd("startinsert!")
+	vim.cmd("startinsert!")
 end
-
---[[
-Explanation:
-
-    os.date is used to format the date and time in Lua.
-    vim.api.nvim_put is the equivalent of put in Vimscript, allowing you to insert lines.
-    vim.api.nvim_win_get_cursor(0) gets the current cursor position in the active window.
-    vim.api.nvim_win_set_cursor(0, position) restores the cursor to its previous position.
-    vim.cmd("startinsert!") switches to insert mode, equivalent to execute "startinsert!" in Vimscript.
-]]
---
 
 -- To use this function, you can map it to a key like this:
 -- vim.api.nvim_set_keymap('n', '<leader>l', ':lua add_log_entry()<CR>', { noremap = true, silent = true })
--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -43,13 +43,18 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          lint.try_lint()
-        end,
-      })
+     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+
+        -- try_lint without arguments runs the linters defined in `linters_by_ft`
+        -- for the current filetype
+        require("lint").try_lint()
+
+        -- You can call `try_lint` with a linter name or a list of names to always
+        -- run specific linters, independent of the `linters_by_ft` configuration
+        require("lint").try_lint("cspell")
+      end,
+    })
     end,
   },
 }
