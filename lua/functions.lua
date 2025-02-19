3695cb62951bc90bd3d89dd4940f096e44e887c9
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2025-02-19 05:40:46 +0100

    Finally made my log command work in nvim

--- lua/functions.lua
+++ lua/functions.lua
@@ -11,62 +11,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
-
---[[
-function! CleanUpTheFile()
-    call feedkeys("gg=G")     " Fix indent
-    call feedkeys("2\<C-o>")  " Jump back to steps in the jump list, which should bring you to where
-                              " before the indent changes
-    %s/\s\+$//e               " Remove trailing whitespaces
-    %s/\r//ge                 " Remove Windows LineEndings
-endfunction
-]]
---
-
---[[
-" Add the current date as markdown headline for a new log entry
-function! AddLogEntry()
-    put =strftime('# %A %d.%m.%Y%n%n%n')
-    let position = getpos(".")
-    put _
-    call setpos('.', position)
-    execute "startinsert!"
-endfunction
-]]
---
-
 function AddLogEntry()
 	-- Insert the formatted date
-	vim.api.nvim_put({ os.date("# %A %d.%m.%Y") .. "\n\n\n" }, "l", true, true)
-	-- Save the current cursor position
-	local position = vim.api.nvim_win_get_cursor(0)
+	vim.api.nvim_put({ "", os.date("# %A %d.%m.%Y") }, "l", false, true)
 	-- Insert an empty line after the date
-	vim.api.nvim_put({ "_" }, "l", true, true)
-	-- Restore the cursor position
-	vim.api.nvim_win_set_cursor(0, position)
+	vim.api.nvim_put({ "", "* ", "", "**WRITE**", "" }, "l", true, true)
+	vim.cmd("norm! 4k")
 	-- Enter insert mode
 	vim.cmd("startinsert!")
 end
-
--- To use this function, you can map it to a key like this:
--- vim.api.nvim_set_keymap('n', '<leader>l', ':lua add_log_entry()<CR>', { noremap = true, silent = true })
-
---[[
-function! AdvClose()
-    :bp | sp | bn | bd
-endfunction
-]]
---
-
---[[
-function! SuperCleverTab()
-    if strpart(getline('.'), 0, col('.') - 1) =~ '^\s*$'
-        return "\<Tab>"
-    elseif pumvisible()
-        return "\<c-n>"
-    else
-        return "\<C-N>"
-    endif
-endfunction
-]]
---
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,12 +5,6 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>",  '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>",    '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>",  '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
@@ -56,3 +50,5 @@ vim.keymap.set("n", "<C-d>", "dd")
 -- don't jump over split lines
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
+
+vim.api.nvim_set_keymap("n", "<leader>l", ":lua AddLogEntry()<CR>", { noremap = true, silent = true })
