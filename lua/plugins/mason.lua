bf466a00fd6ff17470a5a79a4a66b2c529efebc3
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2025-05-15 05:26:05 +0200

    Added kickstart changes to mason and lsp config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,19 @@
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"mason-org/mason.nvim",
+	version = "^1.0.0",
+	flsconfig = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-  end,
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+	end,
 }
