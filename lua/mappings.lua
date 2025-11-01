26f6f3022d11ff1e4622f73b075d48bf3f23b241
Stephan Weintrit <45856463+sweintritt@users.noreply.github.com>
2025-11-01 06:50:46 +0100

    Synced mappings with my vim configuration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,5 +1,8 @@
--- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>ts", ":setlocal spell spelllang=de<CR>", { desc = "[T]oogle [S]pell checking" })
+vim.keymap.set("n", "<leader>rs", ":nohlsearch<CR>", { desc = "[R]eset [S]earch highlighting" })
+
+vim.keymap.set("n", "<C-x>", ":bn<CR>", { desc = "Switch to next buffer" })
+vim.keymap.set("n", "<C-y>", ":bp<CR>", { desc = "Switch to previous buffer" })
 
 -- Keybinds to make split navigation easier.
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
@@ -7,19 +10,11 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
--- Copy to and paste from xterm clipboard
-vim.keymap.set("v", "<C-c>", '"+yi', { desc = "copy line to xterm clipboard" })
-vim.keymap.set("v", "<C-v>", '<ESC>"+p', { desc = "Paste from xterm clipboard" })
-vim.keymap.set("i", "<C-v>", '<ESC>"+pa', { desc = "Pase from xterm clipboard" })
-
-vim.keymap.set("n", "<C-d>", "_dd", { desc = "Delete the current line. Without copying." })
-
-vim.keymap.set("n", "<C-x>", ":bn<CR>", { desc = "Switch to next buffer" })
-vim.keymap.set("n", "<C-y>", ":bp<CR>", { desc = "Switch to previous buffer" })
-
-vim.keymap.set("n", "<leader>ts", ":setlocal spell spelllang=de<CR>", { desc = "[T]oogle [S]pell checking" })
-vim.keymap.set("n", "<C-r>", ":nohlsearch<CR>", { desc = "reset search highlighting" })
-vim.keymap.set("c", "<C-r>", "<C-r>+", { desc = "Paste into commandline" })
+-- Easy movement in commane-line mode
+vim.keymap.set("c", "<C-k>", "<Up>")
+vim.keymap.set("c", "<C-j>", "<Down>")
+vim.keymap.set("c", "<C-h>", "<Left>")
+vim.keymap.set("c", "<C-l>", "<Right>")
 
 -- Easy movement in insert mode
 vim.keymap.set("i", "<C-k>", "<Up>")
@@ -27,27 +22,34 @@ vim.keymap.set("i", "<C-j>", "<Down>")
 vim.keymap.set("i", "<C-h>", "<Left>")
 vim.keymap.set("i", "<C-l>", "<Right>")
 
--- Easy movement in commane-line mode
-vim.keymap.set("c", "<C-k>", "<Up>")
-vim.keymap.set("c", "<C-j>", "<Down>")
-vim.keymap.set("c", "<C-h>", "<Left>")
-vim.keymap.set("c", "<C-l>", "<Right>")
-
+-- Easy help navigation
 vim.keymap.set("n", "<leader><CR>", "<C-]>", { desc = "Follow links in help" })
-vim.keymap.set("n", "<BS>", "<C-T>", { desc = "Jump back in help" })
+vim.keymap.set("n", "<leaser><BS>", "<C-T>", { desc = "Jump back in help" })
+
+-- Copy/paste to/from clipboard
+vim.keymap.set("v", "<C-c>", '"+yi', { desc = "Copy line to clipboard" })
+vim.keymap.set("n", "<C-v>", '<ESC>"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("v", "<C-v>", '<ESC>"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("i", "<C-v>", '<ESC>"+pa', { desc = "Pase from clipboard" })
+-- Paste into commandline
+vim.keymap.set("c", "<C-v>", "<C-r>+", { desc = "Paste from clipboard into commandline" })
+vim.keymap.set("c", "<leader>p", '<C-r>"', { desc = "Paste from clipboard into commandline" })
+
+-- move line up/down
+vim.keymap.set("n", "<C-S-j>", ":m .+1<CR>", { desc = "Move current line down" })
+vim.keymap.set("n", "<C-S-k>", ":m .-2<CR>", { desc = "Move current line up" })
 
 -- don't jump over split lines
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
 
-vim.keymap.set("n", "<leader>ol", ":e ~/Dokumente/notes/log/log.md<CR>", { desc = "[O]pen [L]og" })
-vim.keymap.set("n", "<leader>om", ":e ~/Dokumente/notes/morning_tasks.md<CR>", { desc = "[O]pen [M]orning [T]asks" })
+vim.keymap.set("n", "<leader>cb", ":bp | sp | bn | bd<CR>", { desc = "[C]lose [B]uffer" })
 vim.keymap.set("n", "<leader>ale", ":lua AddLogEntry()<CR>", { desc = "[A]dd [L]og [E]ntry" })
 vim.keymap.set("n", "<leader>st", ":TodoTelescope<CR>", { desc = "[S]earch [T]odos" })
-vim.keymap.set("n", "<leader>cb", ":bp | sp | bn | bd<CR>", { desc = "[C]lose [B]uffer" })
+
 -- Save and quit shortcuts
-vim.keymap.set("n", "<Leader>w", ":w<CR>", { desc = "[W]rite file" })
-vim.keymap.set("n", "<Leader>q", ":q<CR>", { desc = "[Q]uit" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "[W]rite file" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "[Q]uit" })
 
 -- Toggle file tree
 vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { desc = "Toggle [N]eotree" })
